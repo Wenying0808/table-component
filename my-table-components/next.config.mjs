a86eee508d64c382f321335d98ff4f69145a903e
Wenying Chang <changwenyingnl@gmail.com>
2024-11-23 17:42:23 +0100

    modify next config

--- my-table-components/next.config.mjs
+++ my-table-components/next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     "output": "export",
-    "scripts": {
-        "dev": "next dev",
-        "build": "next build",
-        "start": "next start"
-    }
 };
 
 export default nextConfig;
