4d09e00979ea9d6a4e27ea1384c0b0757416c0a0
Wenying Chang <changwenyingnl@gmail.com>
2024-11-25 15:40:24 +0100

    remove base path in navbar component

--- my-table-components/src/app/components/navbar/navbar.tsx
+++ my-table-components/src/app/components/navbar/navbar.tsx
@@ -9,29 +9,30 @@ import Link from 'next/link';
 
 export default function Navbar() {
     const pathname = usePathname();
-    const basePath = process.env.GITHUB_PATH || ''; 
     
     return (
         <div className={styles.navbar}>
-            <IconButton href={`${basePath}/`} color="inherit">
-                <HomeIcon />
-            </IconButton>
+             <Link href={`/`}>
+                <IconButton color="inherit">
+                    <HomeIcon />
+                </IconButton>
+            </Link>
 
             <Link
-                className={`${styles.primary} ${pathname === `${basePath}/pages/table1` ? styles.active : ''}`}
-                href={`${basePath}/pages/table1`}
+                className={`${styles.primary} ${pathname === `/pages/table1` ? styles.active : ''}`}
+                href={`/pages/table1`}
             >
                 Table 1
             </Link>
             <Link
-                className={`${styles.primary} ${pathname === `${basePath}/pages/table2` ? styles.active : ''}`}
-                href={`${basePath}/pages/table2`}
+                className={`${styles.primary} ${pathname === `/pages/table2` ? styles.active : ''}`}
+                href={`/pages/table2`}
             >
                 Table 2
             </Link>
             <Link
-                className={`${styles.primary} ${pathname === `${basePath}/pages/table3` ? styles.active : ''}`}
-                href={`${basePath}/pages/table3`}
+                className={`${styles.primary} ${pathname === `/pages/table3` ? styles.active : ''}`}
+                href={`/pages/table3`}
             >
                 Table 3
             </Link>
